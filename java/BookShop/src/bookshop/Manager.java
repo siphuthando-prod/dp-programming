12c06dd9dc4f97124deb0e5505565dbc25f91b3c
S. X Mabuza <siphuthando84@gmail.com>
2026-07-20 19:21:25 +0200

    Updated Manager class and BookShop class

--- java/BookShop/src/bookshop/Manager.java
+++ java/BookShop/src/bookshop/Manager.java
@@ -18,23 +18,37 @@ public class Manager {
         this.isbn = isbn;
         this.genre = genre;
         this.books = new ArrayList<>(5);
+        addBook(name, title, isbn, genre);
     }
     
-    //PUBLIC METHODS++++++++++++++++++++++++++++++++++++++++
-    public void addBook(String name, String title, String isbn, String genre){
+    //GETTERS
+    public ArrayList getBooks(){
+        return this.books;
+    }
+    
+    //SETTERS
+    public void setBooks(ArrayList<Book> books){
+        this.books = books;
+    }
+    
+    //PRIVATE METHODS++++++++++++++++++++++++++++++++++++++++
+    private void addBook(String name, String title, String isbn, String genre){
        Book book = new Book(name, title, isbn, genre);
        books.add(book);
     }
     
-    public void browseMenu(){
+     //PUBLIC METHODS++++++++++++++++++++++++++++++++++++++++
+    public String  browseMenu(){
+        String output = "";
         for (Book book : books) {
-            System.out.println("+++++++++++++++++++++++++++");
-            System.out.println("Author name: " + book.getName());
-            System.out.println("Book title: " + book.getTitle());
-            System.out.println("ISBN: " + book.getIsbn());
-            System.out.println("Book genre: " + book.getGenre());
-            System.out.println("+++++++++++++++++++++++++++");
+            output +=  "+++++++++++++++++++++++++++" +
+                                                    "\nAuthor name: " + book.getName() +
+                                                    "\nBook title: " + book.getTitle()+
+                                                    "\nISBN: " + book.getIsbn()+
+                                                    "\nBook genre: " + book.getGenre()+
+                                                    "\n+++++++++++++++++++++++++++";
         }
+        return output;
     }
     
     public String searchBook(String name){
@@ -44,5 +58,27 @@ public class Manager {
             }
         }
         return "Author Not Found";
+    }
+    
+    public void removeBook(String title){
+        for (Book book : books) {
+            if(book.getTitle().equalsIgnoreCase(title)){
+                books.remove(book);
+            }
+            else{
+                System.out.println("The book was not found!");
+            }
+        }
+    }
+    
+    public Book updateDetails(String bookTitle, Book book){
+        for (Book book1 : books) {
+            if(book1.getName().equalsIgnoreCase(bookTitle)){
+                book1 = book;
+                return book1;
+            }
+        }
+        
+        return null;
     }
 }
