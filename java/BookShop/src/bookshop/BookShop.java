ee3d735b4a5ad1d9ec9094b307c3be5868fbb5ae
S. X Mabuza <siphuthando84@gmail.com>
2026-07-21 19:52:24 +0200

    Updated BookShop class to include static methods

--- java/BookShop/src/bookshop/BookShop.java
+++ java/BookShop/src/bookshop/BookShop.java
@@ -5,7 +5,38 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BookShop {
-
+//PRIVATE METHODS++++++++++++++++++++++++++++++++++
+    private static String viewMenu(ArrayList<Book> books){
+        String list = "";
+        for (Book book : books) {
+            list += "\n+++++++++++++++++++++++++++++++++++++\n"
+                    + book.getTitle() + "\n++++++++++++++++++++++++++++++++++++++++++"
+                    + "\nAuthor's name: " + book.getName() 
+                    + "\nGenre: " + book.getGenre()
+                    + "\nISBN: " + book.getIsbn() ;
+        }
+        return list;
+    }
+    
+    private static void removeBook(ArrayList<Book> books, String bookTitle) {
+        for (Book book : books) {
+            if(book.getTitle().equalsIgnoreCase(bookTitle)){
+                books.remove(book);
+                System.out.println("Book successfully removed");
+            }
+        }
+    }
+    
+    private static Book updateDetails(ArrayList<Book> books, String bookTitle, Book newBook){
+        for (Book book : books) {
+            if(book.getTitle().equalsIgnoreCase(bookTitle)){
+                book = newBook;
+                return book;
+            }
+        }
+        return null;
+    }
+    //PUBLIC METHODS++++++++++++++++++++++++++++++++++
     public static void main(String[] args) {
         //INSTANTIATING VARIBLES++++++++++++++++++++++
         String name = "";
@@ -17,6 +48,7 @@ public class BookShop {
        ArrayList<Book> books = new ArrayList<>(5);
         Scanner kb = new Scanner(System.in);
         //Manager book = null;
+        Book book = null;
         
         for (int k = 0; k < 5; k++) {
             System.out.print("Enter author name: ");
@@ -29,7 +61,7 @@ public class BookShop {
             isbn = kb.nextLine();
             System.out.println("++++++++++++++++++++++++++");
             
-            Book book = new Book(name, title, isbn, genre);
+            book = new Book(name, title, isbn, genre);
             books.add(book);
         }
         
@@ -43,16 +75,17 @@ public class BookShop {
             genre = kb.nextLine();
         System.out.print("Enter ISBN: ");
             isbn = kb.nextLine();
-        book = new Manager(name, title, isbn, genre);
+        book = new Book(name, title, isbn, genre);
+        books.add(book);
         
         //Veiwing list of books+++++++++++++++++++++++++++++++++
-        String bookMenu = book.browseMenu();
+        String bookMenu = viewMenu(books);
         System.out.println(bookMenu);
         
         //Removing book from the list+++++++++++++++++++++++++++++++
         System.out.print("Enter the name of the book you want to delete:");
         String bookTitle = kb.nextLine();
-        book.removeBook(bookTitle);
+        removeBook(books, bookTitle);
         
         //Changing details of a book+++++++++++++++++++++++++++++++++++
         System.out.print("Enter title of book: ");
@@ -68,8 +101,8 @@ public class BookShop {
         System.out.print("Enter ISBN: ");
         isbn = kb.nextLine();
         
-       Book book1 = new Book(name, title, isbn, genre);
-        Book updated = book.updateDetails(bookTitle, book1);
+       book = new Book(name, title, isbn, genre);
+        Book updated = updateDetails(books, bookTitle, book);
         if(updated != null){
             System.out.println("+++++++++++++++++++++");
             System.out.println("Info Updated Successfully");
@@ -80,7 +113,10 @@ public class BookShop {
             System.out.println("Book genre: " + updated.getGenre());
             System.out.println("+++++++++++++++++++++++++++");
         }
+        else{
+            System.out.println("Book NOT found!");
+        }
         
     }
-    
+
 }
