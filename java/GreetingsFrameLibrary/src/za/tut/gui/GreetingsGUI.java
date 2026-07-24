1a7babca6a912f00a87340c650efbcc713ce1e2e
S. X Mabuza <siphuthando84@gmail.com>
2026-07-24 21:00:02 +0200

    Instantitated Panels in the GreetingsGUI class

--- java/GreetingsFrameLibrary/src/za/tut/gui/GreetingsGUI.java
+++ java/GreetingsFrameLibrary/src/za/tut/gui/GreetingsGUI.java
@@ -2,12 +2,19 @@
 package za.tut.gui;
 
 //IMPORT STATEMENTS++++++++++++++++++++++++++++
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
 
 /**
  *
@@ -20,7 +27,7 @@ public class GreetingsGUI extends JFrame {
         private JPanel nameAndSurnamePnl;
         private JPanel greetingsAreaPnl;
         private JPanel btnsPnl;
-        private JPanel nasnPnl;
+        private JPanel mainPnl;
         private JPanel headingPnl;
         
         //LABELS++++++++++++++++++++++++++++++++++++++++++++++
@@ -40,5 +47,28 @@ public class GreetingsGUI extends JFrame {
         private JButton clearBtn;
         private JButton exitBtn;
         
-        
+        public GreetingsGUI(){
+            //Configuring the GUI++++++++++++++++++++++++++++++++++++
+            setLayout(new BorderLayout());
+            setTitle("Greetings UI");
+            setSize(700, 750);
+            setBackground(Color.YELLOW);
+            
+            //Creating the panels+++++++++++++++++++++++++++++++++++++++
+            namePnl = new JPanel(new FlowLayout());
+            surnamePnl = new JPanel(new FlowLayout());
+            
+            nameAndSurnamePnl = new JPanel(new GridLayout(2, 1));
+            
+            greetingsAreaPnl = new JPanel(new FlowLayout());
+            greetingsAreaPnl.setBorder(new TitledBorder(new LineBorder(Color.BLACK), "Greetings"));
+            
+            btnsPnl = new JPanel(new FlowLayout());
+            mainPnl = new JPanel(new FlowLayout());
+            
+            headingPnl = new JPanel(new FlowLayout(FlowLayout.CENTER));
+            headingPnl.setBorder(new BevelBorder(BevelBorder.RAISED));
+            
+            
+        }
 }
