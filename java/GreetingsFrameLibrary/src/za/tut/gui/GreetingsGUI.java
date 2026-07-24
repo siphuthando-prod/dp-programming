4baa191b65fa76626018dc4a1d5f7ad90704f5d4
S. X Mabuza <siphuthando84@gmail.com>
2026-07-24 20:42:30 +0200

    Created GreetingsGUI frame with Panels, Labels, TextFields, TextAreas, & Buttons

---
+++ java/GreetingsFrameLibrary/src/za/tut/gui/GreetingsGUI.java
@@ -0,0 +1,44 @@
+
+package za.tut.gui;
+
+//IMPORT STATEMENTS++++++++++++++++++++++++++++
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author S. X Mabuza
+ */
+public class GreetingsGUI extends JFrame {
+        //PANELS+++++++++++++++++++++++++++++++++++++++++++++
+        private JPanel namePnl;
+        private JPanel surnamePnl;
+        private JPanel nameAndSurnamePnl;
+        private JPanel greetingsAreaPnl;
+        private JPanel btnsPnl;
+        private JPanel nasnPnl;
+        private JPanel headingPnl;
+        
+        //LABELS++++++++++++++++++++++++++++++++++++++++++++++
+        private JLabel headingLbl;
+        private JLabel nameLbl;
+        private JLabel surnameLbl;
+        
+        //TEXTFIELDS++++++++++++++++++++++++++++++++++++++++
+        private JTextField nameTxtFld;
+        private JTextField surnameTxtFld;
+        
+        //TEXTAREA++++++++++++++++++++++++++++++++++++++++++
+        private JTextArea greetingsTxtArea;
+        
+       //BUTTONS++++++++++++++++++++++++++++++++++++++++++++++
+        private JButton greetBtn;
+        private JButton clearBtn;
+        private JButton exitBtn;
+        
+        
+}
