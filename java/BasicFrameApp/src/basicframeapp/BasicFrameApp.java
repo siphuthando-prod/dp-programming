672960962ef9218fd33a71a6e1410eddb0900d01
S. X Mabuza <siphuthando84@gmail.com>
2026-07-24 19:44:51 +0200

    Created BasicFrameApp class with main method

---
+++ java/BasicFrameApp/src/basicframeapp/BasicFrameApp.java
@@ -0,0 +1,14 @@
+
+package basicframeapp;
+
+/**
+ *
+ * @author S. X Mabuza
+ */
+public class BasicFrameApp {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
