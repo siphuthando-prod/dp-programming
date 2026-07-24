515c59fa4127e62091d00131e52c6cce03794533
S. X Mabuza <siphuthando84@gmail.com>
2026-07-24 19:39:59 +0200

    Added a constructor that sets some attributes of the frame and make it visible

--- java/BasicFrame/src/za/ac/tut/ui/MyFirstFrame.java
+++ java/BasicFrame/src/za/ac/tut/ui/MyFirstFrame.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package za.ac.tut.ui;
+//IMPORT STATEMENTS++++++++++++++++++++++++++++
+import java.awt.Color;
+import javax.swing.JFrame;
 
 /**
  *
  * @author S. X Mabuza
  */
-public class MyFirstFrame {
-    
+public class MyFirstFrame extends JFrame{
+    //CONSTRUCTOR++++++++++++++++++++++++
+    public MyFirstFrame(){
+        setTitle("My First GUI");
+        setSize(200, 250);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setForeground(Color.BLUE);
+        setVisible(true);
+    }
 }
