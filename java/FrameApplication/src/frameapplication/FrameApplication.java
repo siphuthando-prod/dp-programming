d69fe09347a7698b534979e4d72cdac0c9ec8743
S. X Mabuza <siphuthando84@gmail.com>
2026-07-24 20:22:42 +0200

    Created FrameApplication class with main method

---
+++ java/FrameApplication/src/frameapplication/FrameApplication.java
@@ -0,0 +1,17 @@
+
+package frameapplication;
+
+import frames.basicframe.BasicFrame;
+
+/**
+ *
+ * @author S. X Mabuza
+ */
+public class FrameApplication {
+
+    public static void main(String[] args) {
+        // INSTANTIATING FRAME++++++++++++++++++++++++++++
+        BasicFrame frame = new BasicFrame();
+    }
+    
+}
