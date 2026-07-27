60472ea6a929479c6b45eca0c23dff57a6b93eb3
S. X Mabuza <siphuthando84@gmail.com>
2026-07-27 20:44:23 +0200

    Declaring Labels, textfields, combobox, radion buttons, checkbox, buttongroup, textarea,scrollpane, & a private button

--- java/GymMembershipFrameLibrary/src/za/ac/tut/ui/GymMembershipFrame.java
+++ java/GymMembershipFrameLibrary/src/za/ac/tut/ui/GymMembershipFrame.java
@@ -2,8 +2,17 @@
 package za.ac.tut.ui;
 
 //IMPORT STATEMENTS++++++++++++++++++++++++++++++++++++++++++
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
  *
@@ -24,4 +33,43 @@ public class GymMembershipFrame extends JFrame {
     private JPanel headingClientCombinedPnl;
     private JPanel membershipCommentsCombinedPnl;
     private JPanel mainPnl;
+    
+    //Labels+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private JLabel headingLbl;
+    private JLabel nameLbl;
+    private JLabel surnameLbl;
+    private JLabel idNoLbl;
+    private JLabel genderLbl;
+    private JLabel peresonalTrainerLbl;
+    private JLabel contractTypeLbl;
+    
+    //Textfields++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private JTextField nameTxtFld;
+    private JTextField  surnameTxtFld;
+    private JTextField idNoTxtFld;
+    
+    //Combbox++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private JComboBox genderComboBox;
+    
+    //Radio buttons+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private JRadioButton monthToMonthRadBtn;
+    private JRadioButton sixMonthsRadBtn;
+    private JRadioButton annualRadBtn;
+    
+    //Checkbox++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private JCheckBox personalTrainerChkBx;
+    
+    //Buttongroup++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private ButtonGroup btnGrp;
+    
+    //Textarea++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private JTextArea commentsArea;
+    
+    //Scrollpane+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private JScrollPane scrollableTxtArea;
+    
+    //Private button++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private JButton applyBtn;
+    
+    
 }
