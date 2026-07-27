162d8048916682bda35ddbff538814710b16b4ef
S. X Mabuza <siphuthando84@gmail.com>
2026-07-27 09:06:40 +0200

    Created new PasswordApp with a main method
    Instantiated the PasswordGUI frame

---
+++ java/PasswordApp/src/passwordapp/PasswordApp.java
@@ -0,0 +1,17 @@
+
+package passwordapp;
+
+import za.ac.tut.frames.PasswordGUI;
+
+/**
+ *
+ * @author S. X Mabuza
+ */
+public class PasswordApp {
+
+    public static void main(String[] args) {
+        // Instantiating password frame+++++++++++++++++++++++++++++++++++++
+        new PasswordGUI();
+    }
+    
+}
