fd8a5b13273de21e6f67dcb164f40e2a954dbf42
S. X Mabuza <siphuthando84@gmail.com>
2026-07-27 07:40:19 +0200

    Adding Panels

--- java/BasicPasswordFrame/src/za/ac/tut/frames/PasswordGUI.java
+++ java/BasicPasswordFrame/src/za/ac/tut/frames/PasswordGUI.java
@@ -2,11 +2,20 @@
 package za.ac.tut.frames;
 //IMPORT STATEMENTS+++++++++++++++++++++++++++++++++++++++
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  *
  * @author S. X Mabuza
  */
 public class PasswordGUI extends JFrame {
+    //PANELS+++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private JPanel usernamePnl;
+    private JPanel passwordPnl;
+    private JPanel usernameAndPasswordPnl;
+    private JPanel titlePnl;
+    private JPanel headingPnl;
+    private JPanel btnsPnl;
+    private JPanel mainPnl;
     
 }
