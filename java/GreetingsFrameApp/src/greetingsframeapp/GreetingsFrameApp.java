a0e1fd840ac878c5bbcb347bf298c6fbcde187c7
S. X Mabuza <siphuthando84@gmail.com>
2026-07-27 07:16:59 +0200

    Created GreetingsFrameApp class with a main method

---
+++ java/GreetingsFrameApp/src/greetingsframeapp/GreetingsFrameApp.java
@@ -0,0 +1,15 @@
+
+package greetingsframeapp;
+
+/**
+ *
+ * @author S. X Mabuza
+ */
+public class GreetingsFrameApp {
+
+    public static void main(String[] args) {
+        // INSTANTIATING FRAME++++++++++++++++++++++++++++++++++
+        
+    }
+    
+}
