5b0ce355b700fb11d900872b9cb87ed2915a5fb6
S. X Mabuza <siphuthando84@gmail.com>
2026-07-27 20:22:49 +0200

    Created GymMembershipFrame class that extends JFrame

---
+++ java/GymMembershipFrameLibrary/src/za/ac/tut/ui/GymMembershipFrame.java
@@ -0,0 +1,13 @@
+
+package za.ac.tut.ui;
+
+//IMPORT STATEMENTS++++++++++++++++++++++++++++++++++++++++++
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author S. X Mabuza
+ */
+public class GymMembershipFrame extends JFrame {
+    
+}
