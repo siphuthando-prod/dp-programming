e959fa0b766970c642e199610e20f23905181d08
S. X Mabuza <siphuthando84@gmail.com>
2026-07-27 07:47:25 +0200

    Adding Labels

--- java/BasicPasswordFrame/src/za/ac/tut/frames/PasswordGUI.java
+++ java/BasicPasswordFrame/src/za/ac/tut/frames/PasswordGUI.java
@@ -2,6 +2,7 @@
 package za.ac.tut.frames;
 //IMPORT STATEMENTS+++++++++++++++++++++++++++++++++++++++
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -17,5 +18,11 @@ public class PasswordGUI extends JFrame {
     private JPanel headingPnl;
     private JPanel btnsPnl;
     private JPanel mainPnl;
+    
+    //LABELS++++++++++++++++++++++++++++++++++++++++++++++++++
+    private JLabel usernameLbl;
+    private JLabel passwordLbl;
+    private JLabel titleLbl;
+    private JLabel headingLbl;
     
 }
