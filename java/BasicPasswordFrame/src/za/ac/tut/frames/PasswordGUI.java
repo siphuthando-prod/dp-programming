7d37749ba37d5cd32e953136772959928fcd4c7d
S. X Mabuza <siphuthando84@gmail.com>
2026-07-27 07:31:23 +0200

    Created PasswordGUI frame

---
+++ java/BasicPasswordFrame/src/za/ac/tut/frames/PasswordGUI.java
@@ -0,0 +1,12 @@
+
+package za.ac.tut.frames;
+//IMPORT STATEMENTS+++++++++++++++++++++++++++++++++++++++
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author S. X Mabuza
+ */
+public class PasswordGUI extends JFrame {
+    
+}
