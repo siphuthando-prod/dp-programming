4f6c4c6de32e249691510618571c757c9521d0d9
S. X Mabuza <siphuthando84@gmail.com>
2026-07-27 20:28:41 +0200

    Declaring Panels

--- java/GymMembershipFrameLibrary/src/za/ac/tut/ui/GymMembershipFrame.java
+++ java/GymMembershipFrameLibrary/src/za/ac/tut/ui/GymMembershipFrame.java
@@ -3,11 +3,25 @@ package za.ac.tut.ui;
 
 //IMPORT STATEMENTS++++++++++++++++++++++++++++++++++++++++++
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  *
  * @author S. X Mabuza
  */
 public class GymMembershipFrame extends JFrame {
-    
+    //Panels+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private JPanel headingPnl;
+    private JPanel clientPnl;
+    private JPanel namePnl;
+    private JPanel idNoPnl;
+    private JPanel genderPnl;
+    private JPanel contractPnl;
+    private JPanel personalTrainerOptionPnl;
+    private JPanel membershipPnl;
+    private JPanel commentsPnl;
+    private JPanel btnsPnl;
+    private JPanel headingClientCombinedPnl;
+    private JPanel membershipCommentsCombinedPnl;
+    private JPanel mainPnl;
 }
