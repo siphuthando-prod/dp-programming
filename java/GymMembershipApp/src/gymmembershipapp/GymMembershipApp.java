a9528a83b2b23f7d492d7437a070f37fa0a6dae0
S. X Mabuza <siphuthando84@gmail.com>
2026-07-28 09:13:44 +0200

    Created GymMembershipApp class to test the GymMembershipFrame frame

---
+++ java/GymMembershipApp/src/gymmembershipapp/GymMembershipApp.java
@@ -0,0 +1,17 @@
+
+package gymmembershipapp;
+
+import za.ac.tut.ui.GymMembershipFrame;
+
+/**
+ *
+ * @author S. X Mabuza
+ */
+public class GymMembershipApp {
+
+    public static void main(String[] args) {
+        // Instantiating GymMembershipFrame+++++++++++++++++++++++++++++++++++++++++++++
+        new GymMembershipFrame();
+    }
+    
+}
