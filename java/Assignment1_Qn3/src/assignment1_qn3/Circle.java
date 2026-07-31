253cd669414128e189333ff1a6064deff886af0a
S. X Mabuza <siphuthando84@gmail.com>
2026-07-31 03:36:02 +0200

    Created Circle class with private members

---
+++ java/Assignment1_Qn3/src/assignment1_qn3/Circle.java
@@ -0,0 +1,12 @@
+
+package assignment1_qn3;
+
+/**
+ *
+ * @author S. X Mabuza
+ */
+public class Circle {
+    //PRIVATE MEMEBERS++++++++++++++++++++++++++++++++++++++++
+    private int raduis;
+    private String color;
+}
