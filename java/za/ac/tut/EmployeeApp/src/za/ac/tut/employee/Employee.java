6ce072cf6a673b7b7650e2467677ae32c76b279b
S. X Mabuza <siphuthando84@gmail.com>
2026-07-31 18:16:31 +0200

    Created Employee class with getters and setters, and a constructor

---
+++ java/za/ac/tut/EmployeeApp/src/za/ac/tut/employee/Employee.java
@@ -0,0 +1,35 @@
+
+package za.ac.tut.employee;
+
+/**
+ *
+ * @author S. X Mabuza
+ */
+public class Employee {
+    //PRIVATE_MEMBERS+++++++++++++++++++++++++++++++++++++++++++++++++++
+    private String name;
+    private String id;
+	
+//GETTERS AND SETTERS++++++++++++++++++++++++++++++++++++++++++++++++
+public String getName(){
+    return this.name;
+}
+	
+public String getId(){
+    return this.id;
+}
+	
+public void setName(String name){
+    this.name = name;
+}
+	
+public void setId(String id){
+    this.id = id;
+}
+	
+//CONSTRUCTOR+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+public Employee(String name, String id){
+    this.name = name;
+    this.id = id;
+}
+}
