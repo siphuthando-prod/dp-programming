7c5fe0850b643510a9dbcd32038d52fa07102bbd
S. X Mabuza <siphuthando84@gmail.com>
2026-07-31 03:45:17 +0200

    Added getters and setters, constructor(s), and public methods to the Circle class

--- java/Assignment1_Qn3/src/assignment1_qn3/Circle.java
+++ java/Assignment1_Qn3/src/assignment1_qn3/Circle.java
@@ -9,4 +9,42 @@ public class Circle {
     //PRIVATE MEMEBERS++++++++++++++++++++++++++++++++++++++++
     private int raduis;
     private String color;
+
+    //GETTERS AND SETTERS++++++++++++++++++++++++++++++++++
+    public int getRaduis() {
+        return raduis;
+    }
+
+    public void setRaduis(int raduis) {
+        this.raduis = raduis;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    //CONSTRUCTOR(S)+++++++++++++++++++++++++++++++++++++++++++++++
+    public Circle(){
+        this.raduis = 0;
+        this.color = "White";
+    }
+    
+    public Circle(int radius, String color){
+        this.raduis = radius;
+        this.color = color;
+    }
+    
+    // PUBLIC METHODS++++++++++++++++++++++++++++++++++++++++++++++++++
+    public double calcArea(){
+        return Math.PI * Math.pow(this.raduis, 2);
+    }
+    
+    public double calcCircumference(){
+        return 2 * Math.PI + this.raduis;
+    }
+    
 }
