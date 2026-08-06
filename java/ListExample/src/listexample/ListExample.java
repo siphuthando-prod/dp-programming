be92468b9c735a48b0271d5c4b5d96ff8299bd3e
S. X Mabuza <siphuthando84@gmail.com>
2026-08-06 13:05:20 +0200

    Created ListExample class with a main method

---
+++ java/ListExample/src/listexample/ListExample.java
@@ -0,0 +1,18 @@
+
+package listexample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author S. X Mabuza
+ */
+public class ListExample {
+
+    public static void main(String[] args) {
+        //Dclaring variables
+        List<Integer> numbers = new ArrayList<>();
+    }
+    
+}
