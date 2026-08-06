07320331e90a24d22d62f5bd702800aad01ff073
S. X Mabuza <siphuthando84@gmail.com>
2026-08-06 13:22:01 +0200

    Udpdated ListExample class to declare a list of integers, check if the list is empty and displays the elements

--- java/ListExample/src/listexample/ListExample.java
+++ java/ListExample/src/listexample/ListExample.java
@@ -11,8 +11,26 @@ import java.util.List;
 public class ListExample {
 
     public static void main(String[] args) {
-        //Dclaring variables
+        //Dclaring variables++++++++++++++++++++++++++++++++++++++++++++++
         List<Integer> numbers = new ArrayList<>();
+        //List<String> names = new ArrayList<>();
+        
+        //Check if list is empty++++++++++++++++++++++++++++++++++++++
+        if(numbers.isEmpty()){
+            System.out.println("The list is empty");
+            
+            //Adding elements in the list+++++++++++++++++++++++++++++++++++++
+            numbers.add(1); //names.add("sizwe-land");
+            numbers.add(2); //names.add("LiNDA");
+            numbers.add(3); //names.add("HIMTHESUN");
+            numbers.add(4); //names.add("$kywalker");
+            numbers.add(5); //names.add("Trace");
+            
+            //Display the elements+++++++++++++++++++++++++++++++++++++++++++++++
+            numbers.forEach(System.out::println);
+            //names.forEach(System.out::println);
+            
+        }
     }
     
 }
