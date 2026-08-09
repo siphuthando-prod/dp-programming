b1b63dbdf96e7b1b2fc659391c2b57135816dcfb
S. X Mabuza <siphuthando84@gmail.com>
2026-08-09 21:02:25 +0200

    Updated the AlienGame class to include the game Panel and an array of buttons

--- java/AlienGame/src/za/ac/tut/aliengame/AlienGame.java
+++ java/AlienGame/src/za/ac/tut/aliengame/AlienGame.java
@@ -3,6 +3,7 @@ package za.ac.tut.aliengame;
 
 //IMPORT STATEMENTS++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import javax.swing.*;
 
 /**
@@ -13,15 +14,27 @@ public class AlienGame extends JFrame{
     //PRIVATE MEMBERS+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     private JLabel scoreLabel = new JLabel("Score: 0");
     private int score;
+    private JPanel gamePanel = new JPanel();
+    private JButton[] buttons = new JButton[16];
     
     //CONSTRUCTOR++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     public AlienGame(){
+        //Configuring the frame++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         this.setTitle("Catch the alien");
         this.setSize(500, 500);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setLayout(new BorderLayout());
         this.add(this.scoreLabel, BorderLayout.NORTH);
+        //Configuring game buttons+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        this.gamePanel.setLayout(new GridLayout(4, 4));
+        this.add(this.gamePanel, BorderLayout.CENTER);
+        
+        for(int k = 0; k < this.buttons.length; k++){
+            this.buttons[k] = new JButton();
+            this.gamePanel.add(this.buttons[k]);
+        }
+        
         this.setVisible(true);
     }
     
