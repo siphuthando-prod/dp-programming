19e916974b2cdfb1c54d9087f8eb394184e0fc9b
S. X Mabuza <siphuthando84@gmail.com>
2026-08-09 20:50:33 +0200

    Updated the AlienGame class to include private members and configured the label

--- java/AlienGame/src/za/ac/tut/aliengame/AlienGame.java
+++ java/AlienGame/src/za/ac/tut/aliengame/AlienGame.java
@@ -2,6 +2,7 @@
 package za.ac.tut.aliengame;
 
 //IMPORT STATEMENTS++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+import java.awt.BorderLayout;
 import javax.swing.*;
 
 /**
@@ -9,14 +10,19 @@ import javax.swing.*;
  * @author S. X Mabuza
  */
 public class AlienGame extends JFrame{
+    //PRIVATE MEMBERS+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private JLabel scoreLabel = new JLabel("Score: 0");
+    private int score;
+    
     //CONSTRUCTOR++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     public AlienGame(){
         this.setTitle("Catch the alien");
         this.setSize(500, 500);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
+        this.setLayout(new BorderLayout());
+        this.add(this.scoreLabel, BorderLayout.NORTH);
         this.setVisible(true);
-        
     }
     
     //MAIN METHOD++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
