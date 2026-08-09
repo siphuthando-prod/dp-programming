9910e5ecea873955ea977e808169d49ac3d98917
S. X Mabuza <siphuthando84@gmail.com>
2026-08-09 20:32:03 +0200

    Created an AlienGame class that extends JFrame and added a constructor with basic Frame configurations

--- java/AlienGame/src/za/ac/tut/aliengame/AlienGame.java
+++ java/AlienGame/src/za/ac/tut/aliengame/AlienGame.java
@@ -1,10 +1,20 @@
 
 package za.ac.tut.aliengame;
 
+//IMPORT STATEMENTS++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+import javax.swing.*;
+
 /**
  *
  * @author S. X Mabuza
  */
-public class AlienGame {
-    
+public class AlienGame extends JFrame{
+    //CONSTRUCTOR++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    public AlienGame(){
+        this.setTitle("Catch the alien");
+        this.setSize(500, 500);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
 }
