abf14e2ea1f0dcbc8849f7f586ed80e0bbcba3a8
S. X Mabuza <siphuthando84@gmail.com>
2026-08-10 19:21:04 +0200

    Updated the WorkingFileViewerGUI class to include a constructor that configures the frame and created some private methods

--- java/WorkshopFileViewer/src/za/ac/tut/gui/WorkshopFileViewerGUI.java
+++ java/WorkshopFileViewer/src/za/ac/tut/gui/WorkshopFileViewerGUI.java
@@ -1,7 +1,11 @@
 
 package za.ac.tut.gui;
 
+//IMPORT STATEMENTS+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JTextArea;
 
@@ -9,10 +13,47 @@ import javax.swing.JTextArea;
  *
  * @author S. X Mabuza
  */
-public class WorkshopFileViewerGUI {
+public class WorkshopFileViewerGUI extends JFrame{
     //PRIVATE MEMBERS+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     private JMenuItem miOpen;
     private JMenuItem miExit;
     private JLabel lblStatus;
     private JTextArea taWorkshops;
+    
+    //CONSTRUCTOR+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    public WorkshopFileViewerGUI(){
+        //Configuring the frame++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        super("Campus Workshop File Viewer");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        createMenu();
+        createInterface();
+        registerListeners();
+        
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    //PRIVATE METHODS++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private void createMenu() {
+        //Configuring menu++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        
+        this.miOpen = new JMenuItem("Open");
+        this.miExit = new JMenuItem("Exit");
+        
+        fileMenu.add(this.miOpen);
+        fileMenu.addSeparator();
+        fileMenu.add(this.miExit);
+    }
+
+    private void createInterface() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private void registerListeners() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
