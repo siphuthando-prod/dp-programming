974cba00e1b8fa1476cc1bdd2f75c105b32b40a1
S. X Mabuza <siphuthando84@gmail.com>
2026-08-10 19:06:47 +0200

    Created a new GUI called WorkshopFileViewerGUI with some private members.

---
+++ java/WorkshopFileViewer/src/za/ac/tut/gui/WorkshopFileViewerGUI.java
@@ -0,0 +1,18 @@
+
+package za.ac.tut.gui;
+
+import javax.swing.JLabel;
+import javax.swing.JMenuItem;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author S. X Mabuza
+ */
+public class WorkshopFileViewerGUI {
+    //PRIVATE MEMBERS+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private JMenuItem miOpen;
+    private JMenuItem miExit;
+    private JLabel lblStatus;
+    private JTextArea taWorkshops;
+}
