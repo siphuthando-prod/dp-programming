2bc592b77794138549fc05d8d98d94ce228cfb31
S. X Mabuza <siphuthando84@gmail.com>
2026-08-11 12:30:10 +0200

    Added a main method

--- java/MyBasicFrame/src/za/ac/tut/gui/MyFrame.java
+++ java/MyBasicFrame/src/za/ac/tut/gui/MyFrame.java
@@ -27,4 +27,8 @@ public class MyFrame extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e){
         
     }
+    
+    public static void main(String[]args){
+        new MyFrame(); 
+    }
 }
